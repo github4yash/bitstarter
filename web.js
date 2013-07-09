0893aa57a65199a20c48279f7e447ad94e51fe70
Ubuntu <ubuntu@ip-172-31-26-205.us-west-2.compute.internal>
2013-07-09 01:09:26 +0000

    removed path to index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 1!');
-	fs.readFile('./index.html', 'utf8', function (err,data) {
+	fs.readFile('index.html', 'utf8', function (err,data) {
 	  if (err) {
             response.send("Error occured...");
 	    return console.log(err);
